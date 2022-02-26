e240c5f874f876ae1d7462498e81a942e5d6fcde
Nate Sales <nate@natesales.net>
2022-02-25 22:39:46 -0800

    refactor: cleanup

--- server.py
+++ server.py
@@ -1,14 +1,18 @@
 import base64
-import json
 import datetime
+import json
 import logging
 import sys
+import time
+
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO
 from networktables import NetworkTables
+from pymongo import MongoClient
 
 app = Flask(__name__)
 socketio = SocketIO(app)
+db = MongoClient("mongodb://localhost:27017")["feather"]
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -21,20 +25,33 @@ logging.info("Connecting to " + nt_server)
 NetworkTables.initialize(server=nt_server)
 
 
+def nt_change_callback(table, key, value, isNew):
+    # print(f"nt change table: {table} key {key} value {value} new {isNew}")
+    if key == "shotTaken":
+        print("Shot taken!")
+        # TODO: Add document
+
+
+fms = NetworkTables.getTable("FMSInfo")
+feather = NetworkTables.getTable("SmartDashboard/feather")
+feather.addEntryListener(nt_change_callback)
+
+time.sleep(0.5)  # Wait for NetworkTables
+if not NetworkTables.isConnected():
+    logging.error("Unable to connect to NetworkTables server " + nt_server)
+    exit(1)
+
+
 @socketio.on("message")
 def handle_message(data):
     logging.info(data)
-
-
-feather = NetworkTables.getTable("SmartDashboard/feather")
-fms = NetworkTables.getTable("FMSInfo")
 
 
 @socketio.on("ping")
 def pong():
     socketio.emit("matchState", feather.getString("matchState", "disabled").title())
     socketio.emit("matchTimer", str(datetime.timedelta(seconds=round(feather.getNumber("matchTimer", 0)))))
-    socketio.emit("matchNumber", fms.getString("MatchNumber", "UNKNOWN"))
+    socketio.emit("matchNumber", fms.getString("MatchNumber", 0))
 
     alliance_string = "Red" if fms.getBoolean("IsRedAlliance", False) else "Blue"
     socketio.emit("alliance", f"{alliance_string} {round(fms.getNumber('StationNumber', 0))}")
